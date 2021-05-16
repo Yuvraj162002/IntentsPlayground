dcbc9366b1f391a69c0b282d9c4e1e46c27fc3f7
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-05-16 21:20:58 +0530

    Shared Preference

---
+++ app/src/main/java/com/example/counterapp/Constants.java
@@ -0,0 +1,7 @@
+package com.example.counterapp;
+
+public class Constants {
+    public static final String  QUANTITY_KEY = "qty";
+    public static final String SEND_OBJECT = "send";
+    public static final String DATA_KEY = "data";
+}
